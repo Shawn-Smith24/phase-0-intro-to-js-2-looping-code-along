dafedc2dbd653520d250016dffa16d58b78a1f31
shawn <shawnsmith0024@gmail.com>
2022-12-15 13:25:21 -0500

    commit tree 6f114ac1edc69786fbe73f45a07ec11b57ef3f98

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names =['Guadalupe', 'Ollie', 'Aki'];
+function writeCards(names){
+    const greetings = [];
+    for (let i = 0; i < names.length; i++){
+        greetings.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    };
+    return greetings
+};
+writeCards();
+
+
+function countDown(number = 25){
+    while(number >= 0){
+    console.log(number--)};
+};
+countDown();
